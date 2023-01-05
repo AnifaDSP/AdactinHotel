31340b5410773b70523360ecefa9363fad33aeb3
AnifaDSP <smohamedanifa786@gmail.com>
2023-01-05 11:03:53 +0530

    Adactin hotel login

---
+++ src/test/java/org/test/ClassA.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class ClassA {
+	
+	private void login() {
+		System.out.println("login");
+	}
+	private void searchhotel() {
+		System.out.println("searchhotel");
+	}
+	private void selecthotel() {
+		System.out.println("selecthotel");
+	}
+}
